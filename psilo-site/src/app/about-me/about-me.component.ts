0ef61dc07af47a6527ec0ecdc7ee895d9cb28ab2
Loic Lion <loic.lion.contact@gmail.com>
2023-09-29 10:26:58 +0200

    refactor

--- psilo-site/src/app/about-me/about-me.component.ts
+++ psilo-site/src/app/about-me/about-me.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataAboutService } from '../services/data-about.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, OnDestroy {
   aboutMeData: any;
+  private dataSubscription!: Subscription;
 
   constructor(private dataAboutService: DataAboutService) {}
 
   ngOnInit(): void {
+    console.log('Abonnement créé');
     this.dataAboutService.getAboutMe().subscribe((data) => {
       this.aboutMeData = data;
     });
+  }
+
+  ngOnDestroy() {
+    console.log('Abonnement détruit');
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
   }
 }
