b74fccdfe176fb27e21c555679a69b369067cc48
Loic Lion <loic.lion.contact@gmail.com>
2023-10-10 16:16:50 +0200

    fix navbar active

--- psilo-site/src/app/services/facebook-post.service.ts
+++ psilo-site/src/app/services/facebook-post.service.ts
@@ -5,8 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class FacebookPostService {
-  private accessToken = '?????????????????';
-  private pageId = '????????';
+  private accessToken =
+    'EAAEBVaD4zNkBO9WwBTPucUTZBt5v0LzlfIM6JuWtoOU5b8beh04NRyHYmJhw77OEThWUINL65N4UyxIlvcXJezquYSVhSY5jN7IgSnejjSSRKFZAUyWTLW3cxoJ9Af8iFhMznwNh1Mx1BDaryjuVVzRstjSQZCj5YwvjhV3U8ZAi0ZAlV4U40ys73uhvn';
+  private pageId = 'Psiloshock';
 
   constructor(private http: HttpClient) {}
 
