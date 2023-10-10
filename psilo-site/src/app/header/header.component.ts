d85a9b5f27685319fc2fbe8352f3b2b53b8ef6d9
Loic Lion <loic.lion.contact@gmail.com>
2023-10-10 11:27:55 +0200

    design menu burger

--- psilo-site/src/app/header/header.component.ts
+++ psilo-site/src/app/header/header.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
