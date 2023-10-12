638b1050c4d388928e85f035b9572284189ed418
Loic Lion <loic.lion.contact@gmail.com>
2023-10-12 14:41:04 +0200

    ajout event on scroll

--- psilo-site/src/app/header/header.component.ts
+++ psilo-site/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuOpen: boolean = false;
+  isScrolled: boolean = false;
+
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/') {
+          this.isScrolled = false;
+          this.renderer.removeClass(this.el.nativeElement, 'header-scroll');
+        } else {
+          this.isScrolled = true;
+        }
+      }
+    });
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -14,5 +33,18 @@ export class HeaderComponent {
 
   closeMenu() {
     this.isMenuOpen = false;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    if (this.router.url === '/') {
+      const scrollY = window.scrollY;
+      if (scrollY >= window.innerHeight) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+        this.renderer.removeClass(this.el.nativeElement, 'header-scroll');
+      }
+    }
   }
 }
